d3cf5181e7a748415308c327e2e7e0fb04975c6e
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-07 12:04:51 +0530

    Update Assignment_7.sh

--- OSC/007_03_AUG_2020/Assignment_7.sh
+++ OSC/007_03_AUG_2020/Assignment_7.sh
@@ -211,5 +211,13 @@ OPTIONAL
 #	a) Download https://github.com/curran/data/blob/gh-pages/all/geonames_cities10000000.csv using curl command
 #   	b) print name if population is greater than 12691835
 
+#!/bin/bash
+
+echo "Downloading File"
+curl -o geonames.csv https://raw.githubusercontent.com/curran/data/gh-pages/all/geonames_cities10000000.csv
+
+echo "Cities with Population more than 12691835 are: "
+awk '{FS=","} $4 > 12691835 {print $1}' geonames.csv 
+
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------
