e8fcf9e2c584e59db7d56299ad9d1f39f7c7603e
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-13 04:19:07 +0530

    Update TestCustomer.java

--- JAVA/002_12_AUG_2020/src/tester/TestCustomer.java
+++ JAVA/002_12_AUG_2020/src/tester/TestCustomer.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class TestCustomer {
     
-    private static Scanner in = new Scanner(System.in);
+        private static Scanner in = new Scanner(System.in);
     
 	public static void main(String[] args) {
 	    
