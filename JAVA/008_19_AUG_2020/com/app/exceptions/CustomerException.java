2dd3b7b2386c61068bd91a0f2f2496bb9c432037
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-24 00:55:13 +0530

    Delete CustomerException.java

--- JAVA/008_19_AUG_2020/com/app/exceptions/CustomerException.java
+++
@@ -1,8 +0,0 @@
-package com.app.exceptions;
-
-@SuppressWarnings("serial")
-public class CustomerException extends Exception {
-	public CustomerException(String exceptionMessage) {
-		super(exceptionMessage);
-	}
-}
