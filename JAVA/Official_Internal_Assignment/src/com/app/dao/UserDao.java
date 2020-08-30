cdb0dd623aa410d43a85f25a29cf0a5705a45a17
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-30 18:47:27 +0530

    Delete UserDao.java

--- JAVA/Official_Internal_Assignment/src/com/app/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import com.app.enums.UserTypeEnum;
-import com.app.exceptions.UserException;
-
-public interface UserDao {
-
-	UserTypeEnum userLogin(String enteredEmailId, String enteredPassword) throws UserException;
-}
