e08ece87177844392706f60687bf9b71a58e66f0
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-30 18:47:07 +0530

    Delete UserService.java

--- JAVA/Official_Internal_Assignment/src/com/app/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.app.service;
-
-import com.app.enums.UserTypeEnum;
-import com.app.exceptions.UserException;
-
-public interface UserService {
-
-	UserTypeEnum userLoginService(String enteredEmailId, String enteredPassword) throws UserException;
-}
