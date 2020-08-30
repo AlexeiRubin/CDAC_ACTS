ff2ef626650969f85d70cb3fb241d11f98336d00
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-30 18:47:16 +0530

    Delete UserException.java

--- JAVA/Official_Internal_Assignment/src/com/app/exceptions/UserException.java
+++
@@ -1,8 +0,0 @@
-package com.app.exceptions;
-
-@SuppressWarnings("serial")
-public class UserException extends Exception {
-	public UserException(String exceptionMessage) {
-		super(exceptionMessage);
-	}
-}
