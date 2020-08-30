aa6a8d6c46b120f6441fab70dbb6fc19cec575f0
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-08-30 18:47:14 +0530

    Delete BookStoreException.java

--- JAVA/Official_Internal_Assignment/src/com/app/exceptions/BookStoreException.java
+++
@@ -1,8 +0,0 @@
-package com.app.exceptions;
-
-@SuppressWarnings("serial")
-public class BookStoreException extends Exception {
-	public BookStoreException(String exceptionMessage) {
-		super(exceptionMessage);
-	}
-}
