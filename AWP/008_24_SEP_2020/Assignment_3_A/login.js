7b3454e38fbab8c0a32077093345d7917da3b912
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-09-29 19:54:26 +0530

    Delete login.js

--- AWP/008_24_SEP_2020/Assignment_3_A/login.js
+++
@@ -1,9 +0,0 @@
-function validateLogin() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(username == "lol" && password == "lol") {
-        alert("Logged in Successfully");
-        return true;
-    }
-    return false;
-}
