6cad13755a700b241e6cd75f96707e750c89c7ec
Anubhav Kumar <61707136+AlexeiRubin@users.noreply.github.com>
2020-12-09 10:17:01 +0530

    Add files via upload

---
+++ MSDN/002_04_DEC_2020/ArrayOps.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace ArrayService
+{
+    public class ArrayOps
+    {
+        public void BubbleSort(int[] arr)
+        {
+            int temp;
+
+            for (int i = 0 ; i < (arr.Length - 1) ; ++i)
+            {
+                for (int j = 0 ; j < ((arr.Length - i) - 1) ; ++j)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted Using BubbleSort");
+            display(arr);
+        }
+
+        public int arrayPartition(int[] arr, int low, int high)
+        {
+            int i, j, temp1, temp2, pivot = arr[high];
+
+            for (i = (low - 1), j = low ; j < high; ++j)
+            {
+                if (arr[j] < pivot)
+                {
+                    ++i;
+                    temp1 = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp1;
+                }
+            }
+
+            temp2 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp2;
+            
+            return (i + 1);
+        }
+
+        public void QuickSort(int[] arr, int low , int high)
+        {
+            if (low < high)
+            {
+                int part = arrayPartition(arr, low, high);
+
+                QuickSort(arr, low, (part - 1));
+                QuickSort(arr, (part + 1), high);
+            }
+        }
+
+        public void LinearSearch(int[] arr, int ele)
+        {
+            for (int i = 0 ; i < arr.Length ; ++i)
+            {
+                if (arr[i] == ele)
+                {
+                    Console.WriteLine("Element {0} found at position : {1}, using Linear Search ", ele, (i + 1));
+                    return;
+                }
+            }
+            Console.WriteLine("Element {0} NOT found", ele);
+        }
+
+        public void BinarySearchIterative(int[] arr, int ele)
+        {
+            int beg = 0, end = (arr.Length - 1), mid;
+
+            while (beg <= end)
+            {
+                mid = ((beg + end) / 2);
+
+                if (arr[mid] == ele)
+                {
+                    Console.WriteLine("Element {0} found at position : {1}, using Iterative Binary Search ", ele, (mid + 1));
+                    return;
+                }
+                else if (ele > arr[mid])
+                    beg = (mid + 1);
+                else
+                    end = (mid - 1);
+            }
+            Console.WriteLine("Element {0} NOT found", ele);
+        }
+
+        public void BinarySearchRecursive(int[] arr, int ele, int beg, int end)
+        {
+            if (beg > end)
+                Console.WriteLine("Element {0} NOT found", ele);
+            else
+            {
+                int mid = ((beg + end) / 2);
+
+                if (arr[mid] == ele)
+                {
+                    Console.WriteLine("Element {0} found at position : {1}, using Recursive Binary Search", ele, (mid + 1));
+                    return;
+                }
+                else if (ele > arr[mid])
+                    BinarySearchRecursive(arr, ele, (mid + 1), end);
+                else
+                    BinarySearchRecursive(arr, ele, beg, (mid - 1));
+            }
+        }
+
+        public void display(int[] arr) => Array.ForEach(arr, element => Console.WriteLine(element));
+    }
+}
---
+++ MSDN/002_04_DEC_2020/TestArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ArrayService;
+
+namespace ArrayEntity
+{
+    class TestArray
+    {
+        static void Main(string[] args)
+        {
+            ArrayOps ops = new ArrayOps();
+            bool exit = false;
+
+            Console.WriteLine("Enter the number of elements of the array");
+            int N = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[N];
+
+            Console.WriteLine("Enter the elemetns of the array");
+            for(int i = 0 ; i < N ; ++i)
+                arr[i] = Int32.Parse(Console.ReadLine());
+
+            while(!exit)
+            {
+                Console.WriteLine("Enter 1 to Sort Using BubbleSort");
+                Console.WriteLine("Enter 2 to Sort Using QuickSort");
+                Console.WriteLine("Enter 3 to Search Using LinearSearch");
+                Console.WriteLine("Enter 4 to Search Using BinarySearch (Iterative)");
+                Console.WriteLine("Enter 5 to Search Using BinarySearch (Recursive)");
+                Console.WriteLine("Enter 6 to EXIT");
+
+                int ele, choice = Int32.Parse(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        ops.BubbleSort(arr);
+                        break;
+
+                    case 2:
+                        ops.QuickSort(arr, 0, (arr.Length - 1));
+                        Console.WriteLine("Sorted Using QuickSort");
+                        ops.display(arr);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter the element to Search");
+                        ele = Int32.Parse(Console.ReadLine());
+                        ops.LinearSearch(arr, ele);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter the element to Search");
+                        ele = Int32.Parse(Console.ReadLine());
+                        ops.BinarySearchIterative(arr, ele);
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Enter the element to Search");
+                        ele = Int32.Parse(Console.ReadLine());
+                        ops.BinarySearchRecursive(arr, ele, 0 , (arr.Length - 1));
+                        break;
+
+                    case 6:
+                        exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
